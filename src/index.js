9fa28ee29181397fa0da9d6e000df54a5bd75b4d
github username <github email address>
2021-01-25 16:08:10 -0500

    commit tree e374917160fee65ca63ba8c1d548cd5426906e54

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function(){
+    loadImages();
+});
+
+
+
+function loadImage() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+        .then(results => {
+            results.message.forEach(image => addImage(image))
+        });
+}
+
+function addImage(newImgUrl) {
+    let container = document.querySelector('#dog-image-container');
+    let newImage = document.createElement('img');
+    newImage.src = newImgUrl;
+    container.appendChild(newImage);
+}
